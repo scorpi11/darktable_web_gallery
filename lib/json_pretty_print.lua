df69b26bf147763f88c58a50b026dae7693dbfab
Tino Mettler <tino+debian@tikei.de>
2025-09-26 17:08:13 +0200

    Minor coding style change

--- lib/json_pretty_print.lua
+++ lib/json_pretty_print.lua
@@ -77,9 +77,11 @@ For more examples, check out the tests in json_pretty_print_spec.lua.
 
 local M = {}
 
-function M:table_count(T)
+function M:table_count(table)
   local count = 0
-  for _ in pairs(T) do count = count + 1 end
+  for _ in pairs(table) do
+     count = count + 1
+  end
   return count
 end
 
