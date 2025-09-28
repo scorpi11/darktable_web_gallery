e9dc5e2a1b0e58bdf7dc4032174f4653606f3236
Tino Mettler <tino+debian@tikei.de>
2025-09-28 15:02:01 +0200

    Update TODO list

--- web_gallery_export.lua
+++ web_gallery_export.lua
@@ -1,10 +1,14 @@
 --[[
    TODO:
-   - implement "supported" callback to limit export to suited file formats
-   - implement zoom function as present in PhotoSwipe
-   - export thumbnail images instead of using imagemagigk/convert
-   - fix transition of modal image view to not show old images when fading in
-   - change JSON loading to avoid browser warning
+   - JS: touch support https://medium.com/codex/image-slider-using-vanilla-html-css-and-javascript-b95c7fe16955 https://github.com/briquet9/image-slider/tree/main
+   - Lua: remove dir if already existent
+   - Lua: use share_dir once available in the API
+   - Lua: implement "supported" callback to limit export to suited file formats
+   - JS: implement zoom function as present in PhotoSwipe
+   - Lua: export thumbnail images instead of using imagemagigk/convert
+   - JS: fix transition of modal image view to not show old images when fading in
+   - JS: change JSON loading to avoid browser warning
+   - Lua: translations
    - copyright headers
 ]]
 
